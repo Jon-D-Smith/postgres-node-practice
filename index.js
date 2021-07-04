d3f8e8ef9598853809a9bd18334f284a6e26ecc8
Jon Smith <aardvarkjon94@yahoo.com>
2021-07-04 15:53:46 -0500

    Finished base api for users route

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express= require("express")
 const app = express()
 const PORT = 3000
+const db = require('./queries')
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
     res.json({info : 'Node.js, Express, and Postgres API'})
 })
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 app.listen(PORT, () => {
     console.log(`App running on ${PORT}`)
